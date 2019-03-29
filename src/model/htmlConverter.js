cf655606ca7ff6f5a225bef80fe259d88b4a12e8
Roman Terentev <roman.dv.93@gmail.com>
2019-03-29 15:59:43 +1000

    FIX: encoding for input html, pdf opts argument

--- src/model/htmlConverter.js
+++ src/model/htmlConverter.js
@@ -6,11 +6,11 @@ const hyperid = require('hyperid')({ urlSafe: true });
 
 const config = require('../config.js');
 
-module.exports = async (html, puppeteerPDFOpts = {}) => {
+module.exports = async (html, puppeteerPDFOpts) => {
   const pdfPath = path.format({
     dir: os.tmpdir(),
     name: hyperid(),
-    ext: '.pdf'
+    ext: '.pdf',
   });
 
   const opts = puppeteerPDFOpts || config.PDF_OPTS;
@@ -18,10 +18,10 @@ module.exports = async (html, puppeteerPDFOpts = {}) => {
 
   const browser = await puppeteer.launch(config.LAUNCH_OPTS);
   const page = await browser.newPage();
-  await page.setContent(html);
+  await page.setContent(html.toString('utf8'));
   await page.emulateMedia('screen');
   await page
-    .pdf(puppeteerPDFOpts)
+    .pdf(opts)
     .then(pdfBuffer => fs.writeFile(pdfPath, pdfBuffer));
 
   const rStream = fs.createReadStream(pdfPath);
